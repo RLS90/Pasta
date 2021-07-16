d8c718781b365011b552f162f2e92513fa6effad
Ryan <ryanleestarr@gmail.com>
2021-07-16 23:08:13 +1000

    123

--- main.tf
+++ main.tf
@@ -1,24 +1,21 @@
 terraform {
-  required_version = ">=1.0"
-  backend "azurerm" {}
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=2.67.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "RemoteState"
+    storage_account_name = "auauauremotestate123"
+    container_name       = "remstate"
+    key                  = "mystate4.tfstate"
   }
 }
-
+ 
 provider "azurerm" {
+  version = "~>2.0"
   features {}
-  subscription_id = "x"
-  tenant_id       = "x"
-  client_id       = "x"
-  client_secret   = "x"
 }
-
+ 
+data "azurerm_client_config" "current" {}
+ 
 resource "azurerm_resource_group" "resourcegroup1" {
-  name     = "resourcegroup1a"
+  name     = "githubresources"
   location = "Australia Southeast"
 }
+ 
