2cf3cb77cdfb4d07eb04e3fda27766952b25ebc1
Ryan <ryanleestarr@gmail.com>
2021-07-16 23:17:15 +1000

    commit123

--- main.tf
+++ main.tf
@@ -10,13 +10,15 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">=2.67.0"
+    }
+  }
 }
- 
+
 provider "azurerm" {
   version = "~>2.0"
   features {}
 }
- 
+
 #data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "resourcegroup1" {
