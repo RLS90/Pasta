fce4910c44cb524819f9eaf8f92d4eee94b94282
Ryan <ryanleestarr@gmail.com>
2021-07-16 23:31:05 +1000

    59

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pastaaa
+
+- Terraform block defines terraform version to use
+- Backend defines remote state location
+- Required providers defines provider's version
+- Provider block defines a provider to use
+- Resource creation of RG
--- main.tf
+++ main.tf
@@ -1,18 +1,25 @@
-provider "azurerm" {
-  version = "=2.0.0"
-  features {}
-}
-
 terraform {
+  required_version = ">=1.0"
   backend "azurerm" {
     resource_group_name  = "RemoteState"
     storage_account_name = "auauauremotestate123"
     container_name       = "remstate"
-    key                  = "mystate4.tfstate"
+    key                  = "mystate5.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.67.0"
+    }
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg-hello-azure" {
-  name     = "rg-hello-azure"
+  name     = "GitHubActions-RG"
   location = "Australia Southeast"
 }
